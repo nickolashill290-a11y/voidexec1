9400e412180a8eea8273a1173d7c5a5c2a2a5e18
nickolashill290-a11y <nickolashill290@gmail.com>
2026-01-07 22:38:04 -0300

    main.lua

--- main.lua
+++ main.lua
@@ -18,6 +18,26 @@ local SobreosScriptsTab = Window:AddTab({ Title = "Sobre os Scripts", Icon = "ho
 local InfoTab = Window:AddTab({ Title = "Info", Icon = "info" })
 local SettingsTab = Window:AddTab({ Title = "Settings", Icon = "settings" })
 
+local ScriptsBloxTab = Window:AddTab({
+    Title = "ScriptsBlox",
+    Icon = "terminal"
+})
+
+local ScriptsBrainrotTab = Window:AddTab({
+    Title = "ScriptsBrainrot",
+    Icon = "terminal"
+})
+
+local SobreosScriptsTab = Window:AddTab({
+    Title = "Sobre os Scripts",
+    Icon = "home"
+})
+
+local InfoTab = Window:AddTab({
+    Title = "Info",
+    Icon = "info"
+})
+
 ScriptsBloxTab:AddButton({
     Title = "Executar Astral",
     Callback = function()
